fc661a14a63b43f7d5a3b33029cccf0bcff2b4b8
Michelle Kim <pasolre95kim@gmail.com>
2018-08-25 21:50:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,3 +4,15 @@ def begins_with_r(arr)
     string[0] == "r"
   end
 end
+
+def contain_a(arr)
+    arr.select do |string|
+    string.include?("a")
+  end
+end
+   
+def first_wa(arr)
+    arr.detect do |string|
+    string[0,2] == "wa"
+  end
+end
