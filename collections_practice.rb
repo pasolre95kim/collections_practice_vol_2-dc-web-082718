d172275fa0e9be8d6ab4af87285526e959e08ef4
Michelle Kim <pasolre95kim@gmail.com>
2018-08-25 21:52:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,33 +18,31 @@ def first_wa(arr)
 end
 
 def remove_non_strings(arr)
-    arr.delete_if do |string| 
+    arr.delete_if do |string|
     string.is_a?(String) == false
   end
 end
-  # binding.pry  
+  # binding.pry
 def count_elements(array)
-    
+
     array.uniq.each {|i| count = 0
         array.each {|i2| if i2 == i then count += 1 end}
         i[:count] = count}
 end
-    
+
 
 def merge_data(keys, data)
   merged = []
   keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
   merged
 end
-    
+
 def find_cool(cool)
-    cool.select {|i| i.any? {|k,v| v == "cool"}} 
+    cool.select {|i| i.any? {|k,v| v == "cool"}}
 end
-    
+
 def organize_schools(schools)
     locations_hash = {}
     schools.collect {|k,v| locations_hash[v[:location]] = []}
     locations_hash.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1  end}}
 end
-   
-
